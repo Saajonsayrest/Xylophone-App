890c0b8cd74347eb0a214f7f6e22583d3d183273
Sajon <sajonshrestha5@gmail.com>
2022-06-12 18:51:52 +0545

    updated commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class xylophone extends StatelessWidget {
             children: <Widget>[
               buildKey(color: Colors.red, soundNumber: 1),
               buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 4),
+              buildKey(color: Colors.yellow, soundNumber: 3),
               buildKey(color: Colors.green, soundNumber: 4),
               buildKey(color: Colors.teal, soundNumber: 5),
               buildKey(color: Colors.blue, soundNumber: 6),
